258e9be55be022b2ed4b5392d830f706b47992bc
Théotime Donnenfeld <tdonnenfeld@gmail.com>
2025-11-03 22:29:05 +0100

    nvim ui

---
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,56 @@
+return {{
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha",
+      background = { light = "latte", dark = "mocha" },
+      transparent_background = false,
+      term_colors = true,
+      integrations = {
+        treesitter = true,
+        native_lsp = { enabled = true },
+        telescope = true,
+        which_key = true,
+        lualine = true,
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        fidget = true,
+      },
+    })
+    vim.cmd.colorscheme("catppuccin-mocha")
+  end,
+}
+,
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = { options = { theme = "catppuccin" } },
+  },
+  {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+    }
+},
+  {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+}
+}
