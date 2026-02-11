62626749fd9699a6fff9cd7b54787076f12c990e
Théotime Donnenfeld <tdonnenfeld@gmail.com>
2026-02-11 10:27:02 +0100

    nvim cleanup

--- dot_config/nvim/lua/lsp/init.lua
+++
@@ -1,4 +0,0 @@
-require("lsp.ruff")
-require("lsp.pyright")
-require("lsp.vimls")
-require("lsp.lua_ls")
--- dot_config/nvim/lua/lsp/ruff.lua
+++
@@ -1,7 +0,0 @@
-vim.lsp.config("ruff", {
-	{
-		on_attach = function(client, bufnr)
-			client.server_capabilities.hoverProvider = false
-		end,
-	},
-})
