da49f58a6a3d4741ff6d0bed658439d2ce0e40b7
Théotime Donnenfeld <tdonnenfeld@gmail.com>
2026-02-25 23:03:55 +0100

    up

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,20 +1,46 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+vim.g.mapleader = ' '
+vim.o.number = true
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.api.nvim_create_autocmd('UIEnter', {
+  callback = function()
+    vim.o.clipboard = 'unnamedplus'
+  end,
+})
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
-require("config")
-require("lazy").setup("plugins")
+vim.o.cursorline = true
+
+vim.o.scrolloff = 10
+vim.o.list = true
+
+vim.o.confirm = true
+
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+
+vim.keymap.set({ 't', 'i' }, '<A-h>', '<C-\\><C-n><C-w>h')
+vim.keymap.set({ 't', 'i' }, '<A-j>', '<C-\\><C-n><C-w>j')
+vim.keymap.set({ 't', 'i' }, '<A-k>', '<C-\\><C-n><C-w>k')
+vim.keymap.set({ 't', 'i' }, '<A-l>', '<C-\\><C-n><C-w>l')
+vim.keymap.set({ 'n' }, '<A-h>', '<C-w>h')
+vim.keymap.set({ 'n' }, '<A-j>', '<C-w>j')
+vim.keymap.set({ 'n' }, '<A-k>', '<C-w>k')
+vim.keymap.set({ 'n' }, '<A-l>', '<C-w>l')
+
+vim.pack.add({ 'https://github.com/mason-org/mason.nvim' })
+require("mason").setup()
+
+vim.lsp.config['lua_ls'] = {
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      }
+    }
+  }
+}
+vim.lsp.enable('lua_ls')
